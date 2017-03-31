1f3b62f21c618bb05ab3b5fc40de2282fab26bc5
Garik Badalyan <garikb@codeaurora.org>
2017-03-31 16:04:42 -0700

    zenfone3: HIDL: Enable compilation of android.hidl.manager@1.0-java
    android.hidl.manager@1.0-java.jar is used by HAL clients
    to register for IServiceNotification.onRegistration()
    notification.
    
    Fixes: E PackageManager: updateAllSharedLibrariesLPw failed: Package
    com.qualcomm.qcrilmsgtunnel requires unavailable shared library
    android.hidl.manager@1.0-java; failing!
    
    Change-Id: Ibc6f943c5e4b85e50ea2b57ae0f85282c14e0fab

--- device.mk
+++ device.mk
@@ -491,7 +491,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.allocator@1.0
+    android.hidl.allocator@1.0 \
+    android.hidl.manager@1.0-java
 
 # Thermal
 PRODUCT_PACKAGES += \
