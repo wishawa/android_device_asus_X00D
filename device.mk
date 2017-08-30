fdbb8dc956f57d1557beb8eb49a11b46de724b3c
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-08-30 21:04:31 +0800

    mido: add seccomp policy
    Change-Id: I2dcc7b256044dcf8db997359b3c4f785daae4cab

--- device.mk
+++ device.mk
@@ -393,6 +393,10 @@ PRODUCT_PACKAGES += \
     libtar \
     libprotobuf-cpp-lite
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
