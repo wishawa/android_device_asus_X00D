345febc5dd4fc9f947a1d05e91bbfa0f7e99ce5f
Planet-X <planetx.dev@gmail.com>
2018-01-23 21:24:56 +0100

    BoardConfig.mk: Disable sepolicy
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci 
 BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78af000
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE    := 2048
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00008000
@@ -191,8 +192,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 #TARGET_KERNEL_HAVE_EXFAT := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # GPS
 TARGET_NO_RPC := true
