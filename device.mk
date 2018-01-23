63028e462df9298067306785bcb0d90b8af659c8
Planet-X <planetx.dev@gmail.com>
2018-01-23 21:27:20 +0100

    device.mk: Disable ANT+
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -467,11 +467,11 @@ PRODUCT_PACKAGES += \
     mke2fs
 
 # ANT+
-PRODUCT_PACKAGES += \
-    antradio_app \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+#PRODUCT_PACKAGES += \
+#    antradio_app \
+#    AntHalService \
+#    com.dsi.ant.antradio_library \
+#    libantradio
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
