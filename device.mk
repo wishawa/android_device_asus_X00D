e09f68cecfcc40a00c9d0b7f02f95e6e29560d54
Joey <joey@lineageos.org>
2018-01-24 11:24:37 +0100

    zenfone3: build textclassifier
    Change-Id: I50d8173d24c4cab773824107b98929dbbc469783
    Signed-off-by: Joey <joey@lineageos.org>

--- device.mk
+++ device.mk
@@ -457,6 +457,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+
+# TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0 \
