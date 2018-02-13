fc81e68fa7de4460bcb5faf5bda2de6f91f5ebe1
Planet-X <planetx.dev@gmail.com>
2018-02-13 20:04:13 +0100

    Fix fingerprint
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- rootdir/etc/fpservice.sh
+++ rootdir/etc/fpservice.sh
@@ -1,2 +1,15 @@
 #!/system/bin/sh
-start gx_fpd_gxfp3
+FP_module=`getprop ro.hardware.fingerprint`
+HW_module=`getprop ro.config.versatility`
+
+if [ "$FP_module" == 'gxfp3' ]; then
+	start gx_fpd_gxfp3
+elif [ "$FP_module" == 'gxfp5' ]; then
+	start gx_fpd_gxfp3
+elif [ "$FP_module" == 'gxfpa' ]; then
+	start gx_fpd
+fi
+
+if [ "$HW_module" == 'CN' ]; then
+	start TEEService
+fi
