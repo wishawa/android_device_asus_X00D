540991f9f10f8b79533ec4897a70d134548fef33
Planet-X <planetx.dev@gmail.com>
2018-03-08 18:13:13 +0100

    Z017: Restore OSS bluetooth hal
    This reverts commit fcdf3206b1ac746c92f727319505fd24cae7048d.

--- device.mk
+++ device.mk
@@ -155,7 +155,8 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0.so
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
