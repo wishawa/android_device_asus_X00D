45858646b51b68989db4b2a64ea3993ccff6a7c1
Makornthawat Eugene Emery (makorn645) <makornthawateugeneemery@gmail.com>
2018-04-12 21:28:06 +0700

    zenfone3: Nuke sepolicy
    Because I don't have a life.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,7 +211,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+# BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
