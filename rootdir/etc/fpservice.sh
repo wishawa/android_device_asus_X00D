3cd75b123c9d3ead5583b8f26e879d0f472aab70
Planet-X <planetx.dev@gmail.com>
2018-04-20 19:51:08 +0200

    zenfone3: rootdir: Remove TEEService from fpservice.sh
    TEEService is not defined in init.
    
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- rootdir/etc/fpservice.sh
+++ rootdir/etc/fpservice.sh
@@ -3,7 +3,6 @@
 /system/vendor/bin/fpseek
 
 FP_module=`getprop ro.hardware.fingerprint`
-HW_module=`getprop ro.config.versatility`
 
 if [ "$FP_module" == 'gxfp3' ]; then
 	/system/vendor/bin/gx_fpd_gxfp3
@@ -11,8 +10,4 @@ elif [ "$FP_module" == 'gxfp5' ]; then
 	/system/vendor/bin/gx_fpd_gxfp3
 elif [ "$FP_module" == 'gxfpa' ]; then
 	/system/vendor/bin/gx_fpd
-fi
-
-if [ "$HW_module" == 'CN' ]; then
-	start TEEService
 fi
