3039824f6b83e48eefcc0bc5bd46710daa624ad2
Makornthawat Emery <makorn645@users.noreply.github.com>
2018-04-20 20:53:41 +0700

    zenfone3: rootdir: Properly execute fp services
    * This is the only possible way to resolve sepolicy for fingerprint services.
    
    Checkout : f3a6a3607c38ac105fe447c9af9ee01fa7adf101
    * And reflect vendor file changes.

--- rootdir/etc/fpservice.sh
+++ rootdir/etc/fpservice.sh
@@ -1,13 +1,16 @@
-#!/system/bin/sh
+#!/system/vendor/bin/sh
+
+/system/vendor/bin/fpseek
+
 FP_module=`getprop ro.hardware.fingerprint`
 HW_module=`getprop ro.config.versatility`
 
 if [ "$FP_module" == 'gxfp3' ]; then
-	start gx_fpd_gxfp3
+	/system/vendor/bin/gx_fpd_gxfp3
 elif [ "$FP_module" == 'gxfp5' ]; then
-	start gx_fpd_gxfp3
+	/system/vendor/bin/gx_fpd_gxfp3
 elif [ "$FP_module" == 'gxfpa' ]; then
-	start gx_fpd
+	/system/vendor/bin/gx_fpd
 fi
 
 if [ "$HW_module" == 'CN' ]; then
