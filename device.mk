6e5d31006062080fcbe21218ce3695873b7b446c
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2018-04-22 17:54:29 +0700

    zenfone3: configs: Add perf configs
    * Taken from stock 15.0410.1803.53

--- device.mk
+++ device.mk
@@ -391,8 +391,12 @@ PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0 \
     android.hardware.media.omx@1.0-service
 
-# Power
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/gamedwhitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/gamedwhitelist.xml \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
 
+# Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
