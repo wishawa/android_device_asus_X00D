31813ecc33bae70c455bcbfb4e2e2ddd2a197403
makorn645 <makornthawateugeneemery@gmail.com>
2018-04-23 20:30:34 +0530

    zenfone3: configs: Use updated media configs from mido
    Taken from : https://github.com/TeamHorizon/android_device_xiaomi_mido/tree/o/configs

--- device.mk
+++ device.mk
@@ -352,13 +352,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
-# Media
-PRODUCT_PACKAGES += \
-    libmediandk \
-    libcamera2ndk \
-    libmedia_jni \
-    libmedia_helper
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
@@ -373,7 +366,6 @@ PRODUCT_COPY_FILES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libextmedia_jni \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
