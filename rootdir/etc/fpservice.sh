c17a709d5505f1d42891b00624f18f6622d29464
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2018-05-06 11:07:49 +0700

    Revert "zenfone3: rootdir: Increase reliability of fpservice"
    This reverts commit 6af488aea3fa5806563d52b162e6d8abdf1f74b5.

--- rootdir/etc/fpservice.sh
+++ rootdir/etc/fpservice.sh
@@ -4,14 +4,6 @@
 
 FP_module=`getprop ro.hardware.fingerprint`
 
-BOOTSTATE=`getprop sys.boot_completed`
-
-while [ "$BOOTSTATE" != "1" ]
-do
-    /system/bin/sleep 1
-    BOOTSTATE=`getprop sys.boot_completed`
-done
-
 if [ "$FP_module" == 'gxfp3' ]; then
 	/system/vendor/bin/gx_fpd_gxfp3
 elif [ "$FP_module" == 'gxfp5' ]; then
