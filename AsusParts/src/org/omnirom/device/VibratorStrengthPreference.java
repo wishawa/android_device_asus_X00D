9ce1afa04744bac7c27fbe5d80fa9e1f5daf841b
makorn645 <makornthawateugeneemery@gmail.com>
2018-05-19 13:49:03 +0530

    zenfone3: AsusParts: Use Vibrator Settings from op5
    Taken from: https://github.com/omnirom/android_device_oneplus_oneplus5/blob/eb37dc3f3269f218ad6b289e158b720790cbb353/DeviceParts/src/org/omnirom/device/VibratorStrengthPreference.java

--- AsusParts/src/org/omnirom/device/VibratorStrengthPreference.java
+++ AsusParts/src/org/omnirom/device/VibratorStrengthPreference.java
@@ -27,12 +27,10 @@ import android.database.ContentObserver;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.SeekBar;
-import android.widget.TextView;
+import android.widget.Button;
 import android.os.Bundle;
 import android.util.Log;
 import android.os.Vibrator;
-
-import java.util.List;
 
 public class VibratorStrengthPreference extends Preference implements
         SeekBar.OnSeekBarChangeListener {
@@ -41,11 +39,9 @@ public class VibratorStrengthPreference extends Preference implements
     private int mOldStrength;
     private int mMinValue;
     private int mMaxValue;
-    private float offset;
     private Vibrator mVibrator;
-    private TextView mValueText;
 
-    private static final String FILE_LEVEL = "/sys/class/timed_output/vibrator/vtg_level";
+    private static final String FILE_LEVEL = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
     private static final long testVibrationPattern[] = {0,250};
 
     public VibratorStrengthPreference(Context context, AttributeSet attrs) {
@@ -55,11 +51,10 @@ public class VibratorStrengthPreference extends Preference implements
         // #define QPNP_HAP_VMAX_MAX_MV		3596
         mMinValue = 116;
         mMaxValue = 3596;
-        offset = mMaxValue / 100f;
 
         mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         setLayoutResource(R.layout.preference_seek_bar);
-	}
+    }
 
     @Override
     public void onBindViewHolder(PreferenceViewHolder holder) {
@@ -69,8 +64,6 @@ public class VibratorStrengthPreference extends Preference implements
         mSeekBar = (SeekBar) holder.findViewById(R.id.seekbar);
         mSeekBar.setMax(mMaxValue - mMinValue);
         mSeekBar.setProgress(mOldStrength - mMinValue);
-        mValueText = (TextView) holder.findViewById(R.id.current_value);
-        mValueText.setText(Integer.toString(Math.round(mOldStrength / offset)) + "%");
         mSeekBar.setOnSeekBarChangeListener(this);
     }
 
@@ -78,33 +71,32 @@ public class VibratorStrengthPreference extends Preference implements
         return Utils.fileWritable(FILE_LEVEL);
     }
 
-    public static String getValue(Context context) {
-        return Utils.getFileValue(FILE_LEVEL, "1100");
-    }
+	public static String getValue(Context context) {
+		return Utils.getFileValue(FILE_LEVEL, "1100");
+	}
 
-    private void setValue(String newValue, boolean withFeedback) {
-        Utils.writeValue(FILE_LEVEL, newValue);
+	private void setValue(String newValue, boolean withFeedback) {
+	    Utils.writeValue(FILE_LEVEL, newValue);
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
         editor.putString(DeviceSettings.KEY_VIBSTRENGTH, newValue);
         editor.commit();
 	    if (withFeedback) {
             mVibrator.vibrate(testVibrationPattern, -1);
         }
-    }
+	}
 
     public static void restore(Context context) {
         if (!isSupported()) {
             return;
         }
 
-        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_VIBSTRENGTH, "1100"); 
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_VIBSTRENGTH, "1100");
         Utils.writeValue(FILE_LEVEL, storedValue);
     }
 
     public void onProgressChanged(SeekBar seekBar, int progress,
             boolean fromTouch) {
         setValue(String.valueOf(progress + mMinValue), true);
-        mValueText.setText(Integer.toString(Math.round((progress + mMinValue) / offset)) + "%");
     }
 
     public void onStartTrackingTouch(SeekBar seekBar) {
@@ -112,6 +104,6 @@ public class VibratorStrengthPreference extends Preference implements
     }
 
     public void onStopTrackingTouch(SeekBar seekBar) {
-        //NA
+        // NA
     }
 }
