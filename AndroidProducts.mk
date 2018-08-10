403e9ac449c77fa063406aca7ac0e2fbc40f284f
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:47:04 +0100

    zenfone3: Consider lineage_zenfone3 product via the AOSP way
    Change-Id: Ibdeb62f0aa30933a09ac12ca825025e5f18574ab
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,25 +1,17 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Inherit from zenfone3 device
-$(call inherit-product, device/asus/zenfone3/device.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := zenfone3
-PRODUCT_NAME := full_zenfone3
-
-$(call inherit-product-if-exists, vendor/asus/zenfone3/zenfone3-vendor.mk)
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_zenfone3.mk
--- lineage_zenfone3.mk
+++ lineage_zenfone3.mk
@@ -1,23 +1,28 @@
-# Copyright (C) 2016 The CyanogenMod Project
+#
+# Copyright (C) 2015 The CyanogenMod Project
 # Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/asus/zenfone3/full_zenfone3.mk)
+# Inherit from zenfone3 device
+$(call inherit-product, device/asus/zenfone3/device.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := zenfone3
+# Inherit from zenfone3 vendor
+$(call inherit-product-if-exists, vendor/asus/zenfone3/zenfone3-vendor.mk)
+
 PRODUCT_NAME := lineage_zenfone3
+PRODUCT_DEVICE := zenfone3
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 
