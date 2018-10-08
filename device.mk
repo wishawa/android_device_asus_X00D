520fc4e3d8b7640d6d85f4a9d773bfb58f7c9113
Planet-X <planetx.dev@gmail.com>
2018-10-08 16:27:56 +0200

    Revert "zenfone3: Add modified qti_libpermissions.xml to device tree"
    This reverts commit cb9067aad2a449540cf46dca8c6950d12a0f935a.

--- device.mk
+++ device.mk
@@ -52,8 +52,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
-    $(LOCAL_PATH)/permissions/asus.software.zenui.xml:system/etc/permissions/asus.software.zenui.xml \
-    $(LOCAL_PATH)/permissions/qti_libpermissions.xml:system/etc/permissions/qti_libpermissions.xml
+    $(LOCAL_PATH)/permissions/asus.software.zenui.xml:system/etc/permissions/asus.software.zenui.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
