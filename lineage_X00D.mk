98d287423b105a7a70b2beb46c8ac285d4b0f81f
wishawa <wisha.wa@yandex.com>
2020-02-18 21:16:15 +0700

    remove useless files

--- lineage_X00D.mk
+++
@@ -1,8 +0,0 @@
-# Inherit device configuration
-$(call inherit-product, device/asus/X00D/device.mk)
-
-PRODUCT_NAME := lineage_X00D
-PRODUCT_DEVICE := X00D
-PRODUCT_BRAND := asus
-PRODUCT_MANUFACTURER := asus
-PRODUCT_MODEL := ASUS_X00DDA
