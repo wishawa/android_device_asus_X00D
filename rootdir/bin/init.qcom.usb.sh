14ac5e341654e3f0a2692ebe63da9d73b00adce9
wishawa <wisha.wa@yandex.com>
2020-03-14 13:34:54 +0700

    comment out some unneeded lines in init.qcom.usb.sh as they are commmented out in zenfone3's

--- rootdir/bin/init.qcom.usb.sh
+++ rootdir/bin/init.qcom.usb.sh
@@ -112,77 +112,77 @@ fi
 baseband=`getprop ro.baseband`
 
 echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
-usb_config=`getprop persist.sys.usb.config`
-case "$usb_config" in
-    "" | "adb") #USB persist config not set, select default configuration
-      case "$esoc_link" in
-          "PCIe")
-              setprop persist.sys.usb.config diag,diag_mdm,serial_cdev,rmnet_qti_ether,mass_storage,adb
-          ;;
-          *)
-	  case "$baseband" in
-	      "apq")
-	          setprop persist.sys.usb.config diag,adb
-	      ;;
-	      *)
-	      case "$soc_hwplatform" in
-	          "Dragon" | "SBC")
-	              setprop persist.sys.usb.config diag,adb
-	          ;;
-                  *)
-		  soc_machine=${soc_machine:0:3}
-		  case "$soc_machine" in
-		    "SDA")
-	              setprop persist.sys.usb.config diag,adb
-		    ;;
-		    *)
-	            case "$target" in
-                      "msm8916")
-		          setprop persist.sys.usb.config diag,serial_smd,rmnet_bam,adb
-		      ;;
-	              "msm8994" | "msm8992")
-	                  setprop persist.sys.usb.config diag,serial_smd,serial_tty,rmnet_ipa,mass_storage,adb
-		      ;;
-	              "msm8996")
-	                  setprop persist.sys.usb.config diag,serial_cdev,serial_tty,rmnet_ipa,mass_storage,adb
-		      ;;
-	              "msm8909")
-		          setprop persist.sys.usb.config diag,serial_smd,rmnet_qti_bam,adb
-		      ;;
-	              "msm8937")
-			    case "$soc_id" in
-				    "313" | "320")
-				       setprop persist.sys.usb.config diag,serial_smd,rmnet_ipa,adb
-				    ;;
-				    *)
-				       setprop persist.sys.usb.config diag,serial_smd,rmnet_qti_bam,adb
-				    ;;
-			    esac
-		      ;;
-	              "msm8952" | "msm8953")
-		          setprop persist.sys.usb.config diag,serial_smd,rmnet_ipa,adb
-		      ;;
-	              "msm8998" | "sdm660" | "apq8098_latv")
-		          setprop persist.sys.usb.config diag,serial_cdev,rmnet,adb
-		      ;;
-	              "sdm845")
-		          setprop persist.sys.usb.config diag,serial_cdev,rmnet,dpl,adb
-		      ;;
-	              *)
-		          setprop persist.sys.usb.config diag,adb
-		      ;;
-                    esac
-		    ;;
-		  esac
-	          ;;
-	      esac
-	      ;;
-	  esac
-	  ;;
-      esac
-      ;;
-  * ) ;; #USB persist config exists, do nothing
-esac
+# usb_config=`getprop persist.sys.usb.config`
+# case "$usb_config" in
+#     "" | "adb") #USB persist config not set, select default configuration
+#       case "$esoc_link" in
+#           "PCIe")
+#               setprop persist.sys.usb.config diag,diag_mdm,serial_cdev,rmnet_qti_ether,mass_storage,adb
+#           ;;
+#           *)
+# 	  case "$baseband" in
+# 	      "apq")
+# 	          setprop persist.sys.usb.config diag,adb
+# 	      ;;
+# 	      *)
+# 	      case "$soc_hwplatform" in
+# 	          "Dragon" | "SBC")
+# 	              setprop persist.sys.usb.config diag,adb
+# 	          ;;
+#                   *)
+# 		  soc_machine=${soc_machine:0:3}
+# 		  case "$soc_machine" in
+# 		    "SDA")
+# 	              setprop persist.sys.usb.config diag,adb
+# 		    ;;
+# 		    *)
+# 	            case "$target" in
+#                       "msm8916")
+# 		          setprop persist.sys.usb.config diag,serial_smd,rmnet_bam,adb
+# 		      ;;
+# 	              "msm8994" | "msm8992")
+# 	                  setprop persist.sys.usb.config diag,serial_smd,serial_tty,rmnet_ipa,mass_storage,adb
+# 		      ;;
+# 	              "msm8996")
+# 	                  setprop persist.sys.usb.config diag,serial_cdev,serial_tty,rmnet_ipa,mass_storage,adb
+# 		      ;;
+# 	              "msm8909")
+# 		          setprop persist.sys.usb.config diag,serial_smd,rmnet_qti_bam,adb
+# 		      ;;
+# 	              "msm8937")
+# 			    case "$soc_id" in
+# 				    "313" | "320")
+# 				       setprop persist.sys.usb.config diag,serial_smd,rmnet_ipa,adb
+# 				    ;;
+# 				    *)
+# 				       setprop persist.sys.usb.config diag,serial_smd,rmnet_qti_bam,adb
+# 				    ;;
+# 			    esac
+# 		      ;;
+# 	              "msm8952" | "msm8953")
+# 		          setprop persist.sys.usb.config diag,serial_smd,rmnet_ipa,adb
+# 		      ;;
+# 	              "msm8998" | "sdm660" | "apq8098_latv")
+# 		          setprop persist.sys.usb.config diag,serial_cdev,rmnet,adb
+# 		      ;;
+# 	              "sdm845")
+# 		          setprop persist.sys.usb.config diag,serial_cdev,rmnet,dpl,adb
+# 		      ;;
+# 	              *)
+# 		          setprop persist.sys.usb.config diag,adb
+# 		      ;;
+#                     esac
+# 		    ;;
+# 		  esac
+# 	          ;;
+# 	      esac
+# 	      ;;
+# 	  esac
+# 	  ;;
+#       esac
+#       ;;
+#   * ) ;; #USB persist config exists, do nothing
+# esac
 
 # set USB controller's device node
 case "$target" in
