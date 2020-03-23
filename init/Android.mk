08ed7aeb1b93d1327c602037dcd7f4394a1e871e
wishawa <wisha.wa@yandex.com>
2020-03-23 14:04:24 +0700

    X00D init

--- init/Android.mk
+++ init/Android.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,16 +12,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_C_INCLUDES := system/core/init external/selinux/libselinux/include system/core/base/include/
+LOCAL_MODULE := libinit_X00D
+LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
-LOCAL_SRC_FILES := init_zenfone3.cpp
-LOCAL_MODULE := libinit_zenfone3
-LOCAL_STATIC_LIBRARIES := \
-    libbase
+LOCAL_SRC_FILES := init_X00D.cpp
 
 include $(BUILD_STATIC_LIBRARY)
