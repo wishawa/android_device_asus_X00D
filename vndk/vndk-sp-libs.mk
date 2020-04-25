30a851570c98921d224842eae45327b8754e9dfc
wishawa <wisha.wa@yandex.com>
2020-04-26 00:47:04 +0700

    vndk: we don't need graphics.mapper 3.0
    Change-Id: I4f41eadcc113b185df4488cef17cd4f547b1856c

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -2,7 +2,6 @@ VNDK_SP_LIBRARIES := \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.mapper@2.1 \
-    android.hardware.graphics.mapper@3.0 \
     android.hardware.graphics.common@1.0 \
     android.hardware.graphics.common@1.1 \
     android.hardware.graphics.common@1.2 \
